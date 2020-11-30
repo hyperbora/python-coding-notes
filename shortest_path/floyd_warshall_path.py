0af4f6fb7c294e04c1cd246587bec39f7685f211
hyperbora <hyperbora@gmail.com>
2020-11-30 10:39:37 +0900

    add floyd_warshall shortest path

---
+++ shortest_path/floyd_warshall_path.py
@@ -0,0 +1,52 @@
+import sys
+INF = sys.maxsize
+
+
+def floyd_warshall(graph, n):
+    for k in range(1, n + 1):
+        for a in range(1, n + 1):
+            for b in range(1, n + 1):
+                graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+
+def _mock_input():
+    inp = """\
+4
+7
+1 2 4
+1 4 6
+2 1 3
+2 3 7
+3 1 5
+3 4 4
+4 3 2"""
+    for line in inp.split(sep='\n'):
+        yield line
+
+
+if __name__ == "__main__":
+    input = _mock_input()
+    n = int(next(input))
+    m = int(next(input))
+    graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+    # 자기 자신으로 가는 비용은 0
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            if a == b:
+                graph[a][b] = 0
+
+    # 테스트 데이터 입력
+    for _ in range(m):
+        a, b, c = map(int, next(input).split())
+        graph[a][b] = c
+
+    floyd_warshall(graph, n)
+
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            if graph[a][b] == INF:
+                print("-", end=" ")
+            else:
+                print(graph[a][b], end=" ")
+        print()
